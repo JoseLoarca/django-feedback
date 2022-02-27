2496871bbf1bedbfa00a558c579999f68f9a4923
José Loarca <joseloarca97@icloud.com>
2022-02-26 21:33:10 -0600

    file uploads

---
+++ profiles/views.py
@@ -0,0 +1,19 @@
+from django.views.generic.edit import CreateView
+from django.views.generic import ListView
+from .models import UserProfile
+
+
+# Create your views here.
+
+
+class ProfilesView(ListView):
+    model = UserProfile
+    template_name = 'profiles/user_profiles.html'
+    context_object_name = 'profiles'
+
+
+class CreateProfileView(CreateView):
+    template_name = 'profiles/create_profile.html'
+    model = UserProfile
+    fields = '__all__'
+    success_url = '/'
