1d7c9256a8a47a91575551c725865ebb0dc88ea7
José Loarca <joseloarca97@icloud.com>
2022-02-26 22:10:02 -0600

    playing with sessions

--- reviews/views.py
+++ reviews/views.py
@@ -1,3 +1,5 @@
+from django.views import View
+from django.http import HttpResponseRedirect
 from django.views.generic.base import TemplateView
 from django.views.generic import ListView
 from django.views.generic import DetailView
@@ -32,3 +34,19 @@ class ReviewsListView(ListView):
 class SingleReviewView(DetailView):
     template_name = 'reviews/single_review.html'
     model = Review
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        loaded_review = self.object
+        request = self.request
+        favorite_id = request.session.get('favorite_review')
+        context['is_favorite'] = str(favorite_id) == str(loaded_review.id)
+        return context
+
+
+class AddFavoriteView(View):
+
+    def post(self, request):
+        review_id = request.POST['review_id']
+        request.session['favorite_review'] = review_id
+        return HttpResponseRedirect('detail/' + review_id)
